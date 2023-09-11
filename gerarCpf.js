17e63a7cbcac7f74779ac6126b83c879dc68a423
Helio <heliomatos02@gmail.com>
2023-09-10 21:19:03 -0300

    Gerar CPF por unidade federativa

--- gerarCpf.js
+++ gerarCpf.js
@@ -1,18 +1,18 @@
 
 /***
-			 * 0 (RS)- 1 (DF GO MS MT TO)- 2 (AC AM AP PA RO RR)- 3 (CE MA PI)- 4 (AL PB PE
-			 * RN)- 5 (BA SE)- 6 (MG)- 7 (ES RJ)- 8 (SP)- 9 (PR SC).
-			 */
+             * 0 (RS)- 1 (DF GO MS MT TO)- 2 (AC AM AP PA RO RR)- 3 (CE MA PI)- 4 (AL PB PE
+             * RN)- 5 (BA SE)- 6 (MG)- 7 (ES RJ)- 8 (SP)- 9 (PR SC).
+             */
 
-function gerarCpf(){
+function gerarCpf() {
     var cpf = "";
-    for(var i=0;i<8;i++){  
+    for (var i = 0; i < 8; i++) {
         cpf = cpf.concat(Math.floor(Math.random() * 10));
     }
     cpf = cpf.concat(geradorDigitoUF());
     cpf = cpf.concat(calcularDvUm(cpf));
     cpf = cpf.concat(calcularDvDois(cpf));
-    document.getElementById("campoCpfFull").value =cpf;
+    document.getElementById("campoCpfFull").value = cpf;
 }
 
 // calcula o dígito da região fiscal
@@ -25,13 +25,13 @@ function calcularDvUm(cpf) {
     var resto;
     var multi = 1;
     var stringCpf = cpf;
-    for (var i=0; i< cpf.length;i++) {
-        soma += stringCpf.charAt(i)* multi;
+    for (var i = 0; i < cpf.length; i++) {
+        soma += stringCpf.charAt(i) * multi;
         multi += 1;
     }
     resto = soma % 11;
-    resto=verificaRestoZero(resto);
-	return resto;
+    resto = verificaRestoZero(resto);
+    return resto;
 }
 
 function calcularDvDois(cpf) {
@@ -39,18 +39,35 @@ function calcularDvDois(cpf) {
     var resto;
     var multi = 0;
     var stringCpf = cpf;
-    for (var i=0; i< cpf.length;i++) {
+    for (var i = 0; i < cpf.length; i++) {
         soma += stringCpf.charAt(i) * multi;
         multi += 1;
     }
     resto = soma % 11;
-    resto=verificaRestoZero(resto);
+    resto = verificaRestoZero(resto);
     return resto;
 }
 
 function verificaRestoZero(resto) {
-    if(resto==10) {
+    if (resto == 10) {
         return 0;
     }
     return resto;
+}
+
+function gerarCpfUF() {
+    var cpf = "";
+    var select = document.getElementById('uf');
+    if(select.value != null && select.value != ""){
+        for (var i = 0; i < 8; i++) {
+            cpf = cpf.concat(Math.floor(Math.random() * 10));
+        }
+        cpf = cpf.concat(select.value);
+        cpf = cpf.concat(calcularDvUm(cpf));
+        cpf = cpf.concat(calcularDvDois(cpf));
+        document.getElementById("campoCpfUF").value = cpf;
+    }else{
+        alert("Gentileza informar a Unidade Federativa!");    
+    }
+    
 }
